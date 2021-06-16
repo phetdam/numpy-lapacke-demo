864d72571ca8c9c29a9fa95826be9bb92bc5de08
Derek Huang <djh458@stern.nyu.edu>
2021-06-16 04:07:20 -0400

    add LinearRegression to namespace of regression subpackage

--- npy_lapacke_demo/regression/__init__.py
+++ npy_lapacke_demo/regression/__init__.py
@@ -2,3 +2,6 @@ __doc__ = """__init__.py for the regression models subpackage.
 
 .. codeauthor:: Derek Huang <djh458@stern.nyu.edu>
 """
+
+# pylint:disable=import-error
+from _linreg import LinearRegression
