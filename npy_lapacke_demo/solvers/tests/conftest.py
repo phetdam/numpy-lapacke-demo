88ad23e99370959f04aa9c7418c504f62aaa2717
Derek Huang <djh458@stern.nyu.edu>
2021-06-28 22:19:06 -0400

    in solvers/tests: conftest.py: added qp_yesargs fixture, same as qp_noargs but hess, a are fed through a tuple, test_mnewton.py: add test_mnewton_sanity + pass tests

--- npy_lapacke_demo/solvers/tests/conftest.py
+++ npy_lapacke_demo/solvers/tests/conftest.py
@@ -31,9 +31,9 @@ def empty_kwargs(kwargs_keylist):
 
     Returns
     -------
-    dict
+    kwargs : dict
         An empty dict.
-    list
+    kwargs_keylist : list
         List of string keys to drop.
     """
     return {}, kwargs_keylist
@@ -50,10 +50,10 @@ def full_kwargs(kwargs_keylist):
 
     Returns
     -------
-    dict
+    kwargs : dict
         An nonempty dict with more keys than those provided in kwargs_keylist
         but containing only a subset of the keys in kwargs_keylist.
-    list
+    kwargs_keylist : list
         List of string keys to drop.
     """
     # only oi, dare, da are names in kwargs_keylist. both
@@ -67,8 +67,42 @@ def full_kwargs(kwargs_keylist):
     return kwargs, kwargs_keylist
 
 
+@pytest.fixture(scope="session")
+def qp_hess_a(default_seed):
+    """Returns the Hessian and linear terms for a convex quadratic objective.
+
+    The objective is 0.5 * x @ hess @ x + a @ x, so hess is the Hessian.
+
+    Parameters
+    ----------
+    default_seed : int
+        pytest fixture. See top-level package conftest.py.
+
+    Returns
+    -------
+    hess : numpy.ndarray
+        Hessian matrix of the objective, shape (5, 5)
+    a : numpy.ndarray
+        Linear terms of the objective, shape (5,)
+    n_features : int
+        Number of features in the corresponding quadratic optimization problem
+    """
+    # number of features (low-dimensionality problem)
+    n_features = 5
+    # PRNG used to generate the linear terms. can't use default_rng since this
+    # fixture is session scope, while default_rng is function scope.
+    rng = np.random.default_rng(default_seed)
+    # compute hessian, guaranteed to be positive definite
+    hess = make_spd_matrix(n_features, random_state=default_seed)
+    hess += 1e-4 * np.eye(n_features)
+    # use spectral condition of hess for range of uniform linear terms + return
+    cond = np.linalg.cond(hess)
+    a = cond * (-1 + 2 * rng.random(n_features))
+    return hess, a, n_features
+
+
 @pytest.fixture(scope="session", params=["separate", "together"])
-def qp_noargs(default_seed, request):
+def qp_noargs(qp_hess_a, request):
     """An unconstrained convex quadratic problem to minimize using mnewton.
 
     Objective, gradient, and Hessian do not take any arguments.
@@ -79,37 +113,28 @@ def qp_noargs(default_seed, request):
 
     Parameters
     ----------
-    default_seed : int
-        pytest fixture. See top-level package conftest.py.
+    qp_hess_a : tuple
+        pytest fixture. See _qp_hess_a.
     request : _pytest.fixtures.FixtureRequest
         Built-in pytest fixture. See the pytest documentation for details.
 
     Returns
     -------
-    function
+    f_obj : function
         The objective to minimize. If request.param == "separate", returns a
         scalar objective value, while if request.param == "together", returns
         (loss, grad). A convex, quadratic function of 5 variables.
-    numpy.ndarray
+    x0 : numpy.ndarray
         The initial parameter guess, essentially numpy.zeros(5)
-    function or bool
+    f_grad : function or bool
         The gradient of the objective. If request.param == "separate", returns
         a numpy.ndarray shape (5,), while if request.param == "together", is
         not a callable and simply set to True.
-    function
+    f_hess : function
         The Hessian of the objective, returning numpy.ndarray shape (5, 5).
     """
-    # low-dimensionality problem
-    n_features = 5
-    # PRNG used to generate the linear terms. can't use default_rng since this
-    # fixture is session scope, while default_rng is function scope.
-    rng = np.random.default_rng(default_seed)
-    # compute hessian, guaranteed to be positive definite
-    hess = make_spd_matrix(n_features, random_state=default_seed)
-    hess += 1e-4 * np.eye(n_features)
-    # use spectral condition number of hess for range of uniform linear terms
-    cond = np.linalg.cond(hess)
-    a = cond * (-1 + 2 * rng.random(n_features))
+    # get Hessian, linear terms, n_features from _qp_hess_a
+    hess, a, n_features = qp_hess_a
     # define objective, gradient. depends on request.param
     if request.param == "together":
         f_obj = lambda x: (0.5 * x @ hess @ x + a @ x, hess @ x + a)
@@ -120,3 +145,57 @@ def qp_noargs(default_seed, request):
     # define hessian + return f_obj, initial guess, f_grad, f_hess
     f_hess = lambda x: hess
     return f_obj, np.zeros(n_features), f_grad, f_hess
+
+
+@pytest.fixture(scope="session", params=["separate", "together"])
+def qp_yesargs(qp_hess_a, request):
+    """An unconstrained convex quadratic problem to minimize using mnewton.
+
+    Objective, gradient, and Hessian are functions of their last two arguments,
+    which are the Hessian matrix and linear terms of the objective. Essentially
+    identical to the optimization problem defined by qp_noargs.
+
+    All tests depending on this fixture are run twice, first with separate
+    objective and gradient, next with an objective that returns (loss, grad)
+    together, where the gradient is simply True. Initial parameter guess is 0.
+
+    Parameters
+    ----------
+    qp_hess_a : tuple
+        pytest fixture. See _qp_hess_a.
+    request : _pytest.fixtures.FixtureRequest
+        Built-in pytest fixture. See the pytest documentation for details.
+
+    Returns
+    -------
+    f_obj : function
+        The objective to minimize. If request.param == "separate", returns a
+        scalar objective value, while if request.param == "together", returns
+        (loss, grad). A convex, quadratic function of 5 variables.
+    x0 : numpy.ndarray
+        The initial parameter guess, essentially numpy.zeros(5)
+    f_grad : function or bool
+        The gradient of the objective. If request.param == "separate", returns
+        a numpy.ndarray shape (5,), while if request.param == "together", is
+        not a callable and simply set to True.
+    f_hess : function
+        The Hessian of the objective, returning numpy.ndarray shape (5, 5).
+    f_args : tuple
+        The additional positional arguments passed to the objective, gradient,
+        Hessian functions. First element is the Hessian matrix, second
+    """
+    # get Hessian, linear terms, n_features from _qp_hess_a
+    hess, a, n_features = qp_hess_a
+    # form tuple holding hess, a. args to pass to f_obj, f_grad, f_hess
+    f_args = (hess, a)
+    # define objective, gradient. depends on request.param. note that f_obj,
+    # f_grad, f_hess all depend on hess, a.
+    if request.param == "together":
+        f_obj = lambda x, hess, a: (0.5 * x @ hess @ x + a @ x, hess @ x + a)
+        f_grad = True
+    else:
+        f_obj = lambda x, hess, a: 0.5 * x @ hess @ x + a @ x
+        f_grad = lambda x, hess, a: hess @ x + a
+    # define hessian + return f_obj, initial guess, f_grad, f_hess
+    f_hess = lambda x, hess, a: hess
+    return f_obj, np.zeros(n_features), f_grad, f_hess, f_args
---
+++ npy_lapacke_demo/solvers/tests/test_mnewton.py
@@ -0,0 +1,86 @@
+__doc__ = """Tests for the mnewton function provided by _mnewton.
+
+.. codeauthor:: Derek Huang <djh458@stern.nyu.edu>
+"""
+
+from functools import partial
+import numpy as np
+import pytest
+
+# pylint: disable=no-name-in-module
+from .._mnewton import mnewton
+
+
+def test_mnewton_sanity(qp_noargs):
+    """Test input checking of mnewton both when there are arguments/no args
+
+    Parameters
+    ----------
+    qp_noargs : tuple
+        pytest fixture. See local conftest.py.
+    """
+    # get objective, initial guess, gradient, Hessian from qp_noargs
+    f_obj, x0, f_grad, f_hess = qp_noargs
+    # objective must be callable
+    with pytest.raises(TypeError, match="fun must be callable"):
+        mnewton("not callable", x0)
+    # x0 must be convertible to ndarray of type double. PyArray_FROM_OTF will
+    # raise a ValueError here and has a message saying "could not convert."
+    with pytest.raises(ValueError, match="could not convert"):
+        mnewton(f_obj, "not convertible to type double")
+    # can't have empty x0
+    with pytest.raises(ValueError, match="x0 must be nonempty"):
+        mnewton(f_obj, np.empty(0))
+    # x0 must be 1D ndarray
+    with pytest.raises(ValueError, match=r"x0 must have shape \(n_features,\)"):
+        mnewton(f_obj, np.array([[1, 2], [3, 4]]))
+    # args must be tuple if provided
+    with pytest.raises(TypeError, match="tuple"):
+        mnewton(f_obj, x0, args=[])
+    # pytest.raises context for validating jac
+    jac_raises = partial(
+        pytest.raises, TypeError, match="jac must be callable or True"
+    )
+    # jac must be provided and must be callable or True
+    with jac_raises():
+        mnewton(f_obj, x0)
+    with jac_raises():
+        mnewton(f_obj, x0, jac="not callable")
+    with jac_raises():
+        mnewton(f_obj, x0, jac=False)
+    # pytest.raises context for validating hess
+    hess_raises = partial(
+        pytest.raises, TypeError, match="hess must be provided and be callable"
+    )
+    # hess must be provided and must be callable
+    with hess_raises():
+        mnewton(f_obj, x0, jac=f_grad)
+    with hess_raises():
+        mnewton(f_obj, x0, jac=f_grad, hess="not callable")
+    # wrapped pytest.raises context for validating gtol, maxiter, beta positive
+    pos_raises = lambda x: pytest.raises(
+        ValueError, match=rf"{x} must be positive"
+    )
+    # gtol, maxiter, beta must be positive if provided
+    with pos_raises("gtol"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, gtol=0)
+    with pos_raises("maxiter"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, maxiter=0)
+    with pos_raises("beta"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, beta=0)
+    # wrapped pytest.raises context for validating alpha, gamma in (0, 1)
+    unit_raises = lambda x: pytest.raises(
+        ValueError, match=rf"{x} must be in \(0, 1\)"
+    )
+    # alpha and gamma must be in (0, 1)
+    with unit_raises("alpha"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, alpha=0)
+    with unit_raises("alpha"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, alpha=1)
+    with unit_raises("gamma"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, gamma=0)
+    with unit_raises("gamma"):
+        mnewton(f_obj, x0, jac=f_grad, hess=f_hess, gamma=1)
+
+
+#def test_
