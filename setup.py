de6bf8125da204ea7aa8ae6274742cdb4a390c61
Derek Huang <djh458@stern.nyu.edu>
2021-07-10 00:07:16 -0400

    setup.py: remove extra list around scipy entry in install_requires

--- setup.py
+++ setup.py
@@ -184,7 +184,7 @@ def _setup():
         url="https://github.com/phetdam/npy_lapacke_demo",
         packages=find_packages(),
         python_requires=">=3.6",
-        install_requires=["numpy>=1.19.1", ["scipy>=1.5.2"]],
+        install_requires=["numpy>=1.19.1", "scipy>=1.5.2"],
         extras_require={"tests": ["pytest>=6.0.1", "scikit-learn>=0.23.2"]},
         ext_package=__package__,
         ext_modules=_get_ext_modules(os.environ)
